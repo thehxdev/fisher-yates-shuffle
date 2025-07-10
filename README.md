77c511361b722c76b6b319a883b9f0cb2c901478
thehxdev <hossein.khosravi.ce@gmail.com>
2025-07-10 19:37:10 +0330

    made wolfssl rng optional
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 Fisher-Yates shuffle algorithm (also known as as Knuth shuffle) implementation
 using WolfSSL random number generator (RNG).
 
+## Dependencies
+If you want to use WolfSSL RNG, then read [DEPENDENCIES.md](DEPENDENCIES.md) first.
+Otherwise continue to build section.
+
 ## Build
-Read [DEPENDENCIES.md](DEPENDENCIES.md) first. Then:
+Without WolfSSL and with libc `srandom` and `random`:
+```bash
+gcc -std=gnu99 -Og -g3 main.c shuffle.c
+```
+With WolfSSL RNG:
 ```bash
 gcc -std=gnu99 -Og -g3 \
     -I./deps/include -L./deps/lib main.c shuffle.c -l:libwolfssl.a
--- shuffle.c
+++ shuffle.c
@@ -2,33 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <wolfssl/options.h>
-#include <wolfssl/wolfcrypt/settings.h>
-#include <wolfssl/wolfcrypt/random.h>
+#ifdef SHUFFLE_WITH_WOLFSSL
+    #include <wolfssl/options.h>
+    #include <wolfssl/wolfcrypt/settings.h>
+    #include <wolfssl/wolfcrypt/random.h>
+
+    #define rng_t       WC_RNG
+    #define rng_init(r) wc_InitRng((r))
+    #define rng_free(r) wc_FreeRng((r))
+    #define rand_range(rng, min, max) __rand_range((rng), (min), (max))
+#else
+    #include <time.h>
+
+    #define rng_t void*
+    #define rng_init(r) srandom(time(NULL))
+    #define rng_free(r)
+    #define rand_range(rng, min, max) __rand_range(min, max)
+#endif
 
 #include "malloc.h"
 #include "shuffle.h"
 
 struct shuffle_ctx {
-    WC_RNG rng;
+    rng_t rng;
 };
 
-static inline size_t rand_range(WC_RNG *rng, size_t min, size_t max) {
+#ifdef SHUFFLE_WITH_WOLFSSL
+static inline size_t __rand_range(WC_RNG *rng, size_t min, size_t max) {
     size_t r = 0;
     wc_RNG_GenerateBlock(rng, (byte*)&r, sizeof(r));
     return ((r % (max - min + 1)) + min);
 }
+#else
+static inline size_t __rand_range(size_t min, size_t max) {
+    size_t r = random();
+    return ((r % (max - min + 1)) + min);
+}
+#endif
 
 shuffle_ctx_t shuffle_ctx_init(void) {
     struct shuffle_ctx *ctx = calloc(1, sizeof(*ctx));
-    wc_InitRng(&ctx->rng);
+    rng_init(&ctx->rng);
     return ctx;
 }
 
-
 void shuffle_ctx_destroy(shuffle_ctx_t _ctx) {
     struct shuffle_ctx *ctx = (struct shuffle_ctx*)_ctx;
-    wc_FreeRng(&ctx->rng);
+    rng_free(&ctx->rng);
     free(ctx);
 }
 
@@ -48,14 +68,15 @@ int shuffle(shuffle_ctx_t _ctx, void *items, size_t item_size,
 int shuffle_simple(void *items, size_t item_size,
                    size_t n_items, __swap_fn swap)
 {
-    WC_RNG rng;
     size_t i, j, idx;
     unsigned char *ptr = (unsigned char*)items;
-    wc_InitRng(&rng);
+    struct shuffle_ctx *ctx = shuffle_ctx_init();
+
     for (i = n_items - 1; i > 0; i--) {
-        j = rand_range(&rng, 0, i);
+        j = rand_range(&ctx->rng, 0, i);
         swap(ptr+(i*item_size), ptr+(j*item_size));
     }
-    wc_FreeRng(&rng);
+
+    shuffle_ctx_destroy(ctx);
     return 0;
 }
